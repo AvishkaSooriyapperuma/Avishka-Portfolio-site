cf7123e515daf3f64ccdae2dff17781e72b3a1e4
Avishka Sooriyapperuma <71138570+AvishkaSooriyapperuma@users.noreply.github.com>
2023-09-30 20:28:16 +0530

    Update index.js

--- avishka_portfolio_site/pages/index.js
+++ avishka_portfolio_site/pages/index.js
@@ -6,7 +6,7 @@ export default function Home() {
   return (
     <div className={styles.container}>
       <h1>
-      avishkasoori:$ <br/> <span className={styles.help}>type help to start&quot;</span>
+      avishkasoori:$ <br/> <span className={styles.help}>type &quot;help&quot; to start</span>
       </h1>
 
 
